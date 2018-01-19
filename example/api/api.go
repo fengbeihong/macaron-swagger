62a3074b6001b14d0cc452576247957b41f1212f
fengbeihong <fengbeihong@gmail.com>
2018-01-19 09:16:21 +0800

    Update commont annotations with swagger 2.0 spec

--- example/api/api.go
+++ example/api/api.go
@@ -6,6 +6,25 @@ import (
 )
 
 
+// swagger:operation GET /testapi api TestAPI
+//
+// test api for swagger 2.0
+//
+// ---
+// produces:
+// - application/json
+// - text/html
+// parameters:
+// - name: param
+//   in: path
+//   description: param
+//   required: false
+//   type: string
+// responses:
+//   '200':
+//     description: login response
+//     schema:
+//       type: string
 func TestAPI(ctx *macaron.Context) {
 	ctx.WriteHeader(http.StatusOK)
 	ctx.Write([]byte("OK"))
--- example/main.go
+++ example/main.go
@@ -1,3 +1,22 @@
+// Package classification Test API.
+//
+// the purpose of this application is to provide an application
+// that is using plain go code to define an API
+//
+// This should demonstrate all the possible comment annotations
+// that are available to turn go code into a fully compliant swagger 2.0 spec
+//
+// Terms Of Service:
+//
+// http://swagger.io/terms/
+//
+//     Schemes: http
+//     Host: 127.0.0.1:4000
+//     Version: 1.0
+//     License: Apache 2.0 http://www.apache.org/licenses/LICENSE-2.0.html
+//     Contact: API Support<support@swagger.io> http://www.swagger.io/support
+//
+// swagger:meta
 package main
 
 import (
